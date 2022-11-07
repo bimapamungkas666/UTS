68e15c2845506bd7e42dc9269ac33daf6fd5fb24
bimapamungkas666 <115439098+bimapamungkas666@users.noreply.github.com>
2022-11-07 21:22:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home():
 def predict():
 	filename = "https://raw.githubusercontent.com/bimapamungkas666/UTS/main/hasil.csv"
 	df = pd.read_csv(filename)
-	df.drop(columns=['gacoan_name', 'name'], inplace=True)
+	df.drop(columns=['wisata_name', 'name'], inplace=True)
 
 	import string
 	import re
